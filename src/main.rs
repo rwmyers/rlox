94a70c44c21701a63cac3d9d9808613b5d45074e
Russell Myers <mezner@russellmyers.com>
2024-05-16 20:50:38 -0400

    Handle constant instructions.
    This should take us up to 14.6.
    
    https://craftinginterpreters.com/chunks-of-bytecode.html

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::convert::TryFrom;
 
+type Value = f64;
+
 /// Operation code.
 #[repr(u8)]
 enum OpCode {
-    Return = 0,
+    Constant = 0,
+    Return = 1,
 }
 
 impl TryFrom<u8> for OpCode {
@@ -11,7 +14,8 @@ impl TryFrom<u8> for OpCode {
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
-            0 => Ok(OpCode::Return),
+            0 => Ok(OpCode::Constant),
+            1 => Ok(OpCode::Return),
             _ => Err("Invalid OpCode value"),
         }
     }
@@ -20,17 +24,24 @@ impl TryFrom<u8> for OpCode {
 
 struct Chunk {
     code: Vec<u8>,
+    constants: Vec<Value>,
 }
 
 impl Chunk {
     fn new() -> Self {
         Chunk {
             code: Vec::new(),
+            constants: Vec::new(),
         }
     }
 
     fn write(&mut self, byte: u8) {
         self.code.push(byte);
+    }
+
+    fn add_constant(&mut self, value: Value) -> usize{
+        self.constants.push(value);
+        self.constants.len() - 1
     }
 
     fn disassemble(&self, name: &str) {
@@ -48,6 +59,9 @@ impl Chunk {
         let instruction = self.code[offset];
         let op_code = OpCode::try_from(instruction).unwrap();
         match op_code {
+            OpCode::Constant => {
+                constant_instruction("OP_CONSTANT", &self, offset)
+            }
             OpCode::Return => {
                 simple_instruction("OP_RETURN", offset)
             }
@@ -60,8 +74,25 @@ fn simple_instruction(name: &str, offset: usize) -> usize {
     offset + 1
 }
 
+fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
+    let constant = chunk.code[offset + 1] as usize;
+    print!("{:<16} {:>4} '", name, constant);
+    print_value(&chunk.constants[constant]);
+    print!("'\n");
+    offset + 2
+}
+
+fn print_value(value: &Value) {
+    print!("{:.6}", value);
+}
+
 fn main() {
     let mut chunk = Chunk::new();
+
+    let constant = chunk.add_constant(1.2f64);
+    chunk.write(OpCode::Constant as u8);
+    chunk.write(constant as u8);
+
     chunk.write(OpCode::Return as u8);
     chunk.disassemble("test chunk");
 }
