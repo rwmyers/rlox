7844f3176173e8b4531b7f1f31fda0189145f8ba
Russell Myers <mezner@russellmyers.com>
2024-05-16 20:02:43 -0400

    Basic Chunk and OpCode implementation.
    This takes us to the beginning of 14.4.
    
    https://craftinginterpreters.com/chunks-of-bytecode.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+/// Operation code.
+enum OpCode {
+    Return,
+}
 
+
+struct Chunk {
+    code: Vec<u8>,
+}
+
+impl Chunk {
+    fn new() -> Self {
+        Chunk {
+            code: Vec::new(),
+        }
+    }
+
+    fn write(&mut self, byte: u8) {
+        self.code.push(byte);
+    }
+}
+
+fn main() {
+    let mut chunk = Chunk::new();
+    chunk.write(OpCode::Return as u8);
 }
